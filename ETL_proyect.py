2166af43d369078e22ca0d2376e866507d44833a
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2024-07-19 09:55:49 -0500

    other funtion

--- ETL_proyect.py
+++ ETL_proyect.py
@@ -41,6 +41,12 @@ def Extract(url, table_attribs):
 def Transform(df,columns_drop):
     '''funtion to delet some columns'''
     df_transformed = df.drop(columns_drop, axis=1)
+    df_transformed['ingresos(usd)'] = df_transformed['ingresos(usd)'].str.replace(',', '').astype(float) # to remove ','
+    df_transformed['ingresos(usd)'] = pd.to_numeric(df_transformed['ingresos(usd)']) # to convert the string to float
+
+    df_transformed['ingresos(eur)'] = df_transformed['ingresos(usd)'] * 0.92 # 1 dollar = 0.92 euros today (19/07/2024)
+    df_transformed['ingresos(cop)'] = df_transformed['ingresos(usd)'] * 4016.19# 1 dollar = 4016.19 pesos colombianos today (19/07/2024)
+
     load_massege('Succesful transformation')
     return df_transformed
 
@@ -52,16 +58,28 @@ def load_CSV(df,output_path):
     df.to_csv(output_path, index=False)
     load_massege('Succesful load')
 
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final data frame to a database
+    table with the provided name. Function returns nothing.'''
+    df.to_sql(table_name,sql_connection, if_exists='replace', index=False)
+    load_massege('Data loaded to Database as a table')
+
 
 '''Here, you define the required entities and call the relevant
 functions in the correct order'''
 url = 'https://es.wikipedia.org/wiki/Anexo:Empresas_más_grandes_de_América_Latina_por_ingresos'
+
 load_massege('the start')
+
 df = Extract(url,['nombre','razon','industria','ingresos(usd)','sede','fecha_reporte'])
 print(df)
+
 df = Transform(df,['razon','sede','fecha_reporte'])
 print(df)
+
 load_CSV(df,'Transformed_File.csv')
+sql_connection = sqlite3.connect('database.db')
+load_to_db(df, sql_connection, 'Empresas más grandes por ingresos')
 
 
 
